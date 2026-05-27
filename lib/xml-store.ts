29a810a6bb49585d00e9063cdf3942a37164e9c5
yangxu128 <1419644549@qq.com>
2026-05-27 15:22:21 +0800

    fix: remove xml-store localStorage persistence to avoid quota exceeded error

--- lib/xml-store.ts
+++ lib/xml-store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface XmlStore {
   headers: string[]
@@ -12,23 +11,16 @@ interface XmlStore {
 }
 
 export const useXmlStore = create<XmlStore>()(
-  persist(
-    (set) => ({
-      headers: [],
-      data: [],
-      fileName: '',
-      loaded: false,
+  (set) => ({
+    headers: [],
+    data: [],
+    fileName: '',
+    loaded: false,
 
-      loadXml: (headers, data, fileName) =>
-        set({ headers, data, fileName, loaded: true }),
+    loadXml: (headers, data, fileName) =>
+      set({ headers, data, fileName, loaded: true }),
 
-      clearData: () =>
-        set({ headers: [], data: [], fileName: '', loaded: false }),
-    }),
-    {
-      name: 'xml-storage',
-      storage: createJSONStorage(() => localStorage),
-      partialize: (state) => ({ headers: state.headers, data: state.data, fileName: state.fileName, loaded: state.loaded }),
-    }
-  )
+    clearData: () =>
+      set({ headers: [], data: [], fileName: '', loaded: false }),
+  })
 )
